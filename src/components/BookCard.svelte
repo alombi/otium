6667e982db9b6c89cd13b74b3a254ac18aedf3d7
alombi <alombi@hotmail.com>
2021-11-07 18:55:11 +0100

    Adds grid view on mobile (closes #2)

--- src/components/BookCard.svelte
+++ src/components/BookCard.svelte
@@ -1,13 +1,20 @@
 <script>
    export let book;
+   let url = `/books/${book.id}`;
    let author = book.volumeInfo.authors ? book.volumeInfo.authors[0] : `Unknown`;
    let cover = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.smallThumbnail : 'https://bookstoreromanceday.org/wp-content/uploads/2020/08/book-cover-placeholder.png'
    let title = book.volumeInfo.title
+   let publisher = book.volumeInfo.publisher
+   let year = book.volumeInfo.publishedDate ? book.volumeInfo.publishedDate.split('-')[0] : 'Unknown'
 </script>
 
 
 <div class="book-card">
-   <img src={cover} alt='cover'>
-   <p class="title">{title}</p>
-   <p class="author">by <i>{author}</i></p>
+   <a href={url}>
+      <img src={cover} alt='cover'>
+      <p class="title">{title}</p>
+      <p class="author">by <i>{author}</i></p>
+      <p>{publisher}, {year}</p>
+   </a>
+   
 </div>
