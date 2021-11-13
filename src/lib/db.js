0bef095c4ef0e64c3a440d738d87e5c60139f039
alombi <alombi@hotmail.com>
2021-11-13 22:43:05 +0100

    Trying to fix env vars

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-   import.meta.env.VITE_SUPABASE_URL,
-   import.meta.env.VITE_SUPABASE_ANON_KEY
+   //import.meta.env.VITE_SUPABASE_URL,
+   //import.meta.env.VITE_SUPABASE_ANON_KEY
+   process.env.VITE_SUPABASE_URL,
+   process.env.VITE_SUPABASE_ANON_KEY
 )
 
 export default supabase
--- src/routes/api/book-[id].js
+++ src/routes/api/book-[id].js
@@ -1,6 +1,7 @@
 export async function get({ params }) {
    const id = params.id
-   const key = import.meta.env.VITE_GOOGLE_BOOKS_KEY
+   //const key = import.meta.env.VITE_GOOGLE_BOOKS_KEY
+   const key = process.env.VITE_GOOGLE_BOOKS_KEY
    const url = `https://www.googleapis.com/books/v1/volumes/${id}?key=${key}`
    let req = await fetch(url)
    var data = await req.json()
