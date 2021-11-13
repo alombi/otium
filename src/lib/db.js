f19aee9668c2a8856ed312a5ae64384880257b29
alombi <alombi@hotmail.com>
2021-11-14 00:43:50 +0100

    Testing vercel deployment

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-   process.env.VITE_SUPABASE_URL,
+   "https://alvntboybhsaexjtmsum.supabase.co",
    process.env.VITE_SUPABASE_ANON_KEY
 )
 
