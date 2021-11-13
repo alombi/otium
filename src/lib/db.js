997a65bafbf0078b18aae03b4c02766ff1e0d433
alombi <alombi@hotmail.com>
2021-11-14 00:23:29 +0100

    Fixed a typo

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
    process.env.VITE_SUPABASE_URL,
-   process.meta.env.VITE_SUPABASE_ANON_KEY
+   process.env.VITE_SUPABASE_ANON_KEY
 )
 
 export default supabase
