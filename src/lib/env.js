72aa64e91e6160597a15765101e208f9a96ae738
alombi <alombi@hotmail.com>
2021-11-14 00:59:15 +0100

    Testing

---
+++ src/lib/env.js
@@ -0,0 +1,3 @@
+export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+export const GOOGLE_BOOKS_KEY = import.meta.env.VITE_GOOGLE_BOOKS_KEY;
