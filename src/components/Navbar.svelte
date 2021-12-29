763ebae41e419b935ed3d3e4447de05d822cea6a
alombi <alombi@hotmail.com>
2021-12-30 00:59:36 +0100

    Adds feedback and indicators in login process from navbar (#17)

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,6 +1,7 @@
 <script>
   import supabase from '$lib/db';
   import { session } from '$app/stores';
+  import { Jumper } from 'svelte-loading-spinners'
 
   function toggleSidebar(){
     if(document.getElementById('sidebar').style.display == 'none'){
@@ -17,12 +18,17 @@
 
   let email;
   async function signIn(){
+    document.getElementById('loginInNavbar').style.display = 'none';
+    document.getElementById('waitingForEmailToBeSentIndicator').style.display = 'block';
     document.getElementById('loginButton').disabled = 'true';
     const { user, session, error } = await supabase.auth.signIn({
       email: email
     })
     if(error){
       alert(error.message)
+    }else{
+      document.getElementById('waitingForEmailToBeSentIndicator').style.display = 'none';
+      document.getElementById('emailSentIndicator').style.display = 'block'
     }
   }
   async function logOut(){
@@ -43,11 +49,19 @@
     {#if $session}
       <button on:click={logOut} class="buttonAuth">Log out</button>
     {:else}
-    <form class="" on:submit|preventDefault={signIn}> 
-      <input type="text" class="textForm navForm" placeholder="Email address" required="required"  bind:value={email}>
-      <button id="loginButton" class="buttonAuth" type="submit">Sign in</button>
-      <button on:click={()=>window.location.href ='/auth'} id="alternativeButton" class="buttonAuth">Sign in</button>
-    </form>
+      <form id="loginInNavbar" on:submit|preventDefault={signIn}> 
+        <input type="text" class="textForm navForm" placeholder="Email address" required="required"  bind:value={email}>
+        <button id="loginButton" class="buttonAuth" type="submit">Sign in</button>
+        <button on:click={()=>window.location.href ='/auth'} id="alternativeButton" class="buttonAuth">Sign in</button>
+      </form>
+      <div id="waitingForEmailToBeSentIndicator"><Jumper size="40" color="#f2b3cf" unit="px" duration="1s"></Jumper></div>
+      <p id="emailSentIndicator">Email sent!</p>
     {/if}
   </div>
 </nav>
+
+<style>
+  #emailSentIndicator, #waitingForEmailToBeSentIndicator{
+    display: none;
+  }
+</style>
