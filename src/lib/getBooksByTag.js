2c7a91af501983af91c8ae7e9a73ecb6ed4d6044
alombi <alombi@hotmail.com>
2022-02-26 17:16:44 +0100

    Looking for a fix

--- src/lib/getBooksByTag.js
+++ src/lib/getBooksByTag.js
@@ -2,13 +2,17 @@ import supabase from '$lib/db'
 
 export async function getBooksByTag(tag) {
    let DBbookshelf;
-   setTimeout(async () => {
+   try {
       const session = supabase.auth.session()
       const id = session.user.id
       const { data, error } = await supabase.from('profiles').select('bookshelf').eq('id', id)
       DBbookshelf = data[0].bookshelf.filter(function (e) {
          return e.tag == tag
       })
-      return DBbookshelf
-   }, 500)
+   }catch(e) {
+      console.log(e);
+      DBbookshelf = []
+   }
+   return DBbookshelf
+
 }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -57,7 +57,7 @@
    onMount(()=>{
       setTimeout(()=>{
          loaded = loading ()
-      }, 100)
+      }, 500)
    })
 </script>
 
