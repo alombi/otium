123dadb593ee416941f4e52590017d00a6e1a20a
alombi <62696529+alombi@users.noreply.github.com>
2022-03-22 09:32:58 +0100

    Auth test

--- src/routes/auth.svelte
+++ src/routes/auth.svelte
@@ -4,20 +4,20 @@
    import { onMount } from "svelte";
    import { Jumper } from 'svelte-loading-spinners';
    import supabase from '$lib/db';
-   let email;
+   let email, password;
    async function signIn(){
-      document.getElementById('loginFromPage').style.display = 'none';
-      document.getElementById('waitingForEmailToBeSentIndicatorFromAuth').style.display = 'flex';
-      document.getElementById('waitingForEmailToBeSentIndicatorFromAuth').style.justifyContent = 'center';
-      document.getElementById('loginButton').disabled = 'true';
-      const { user, session, error } = await supabase.auth.signIn({
-         email: email
+      //document.getElementById('loginFromPage').style.display = 'none';
+      //document.getElementById('waitingForEmailToBeSentIndicatorFromAuth').style.display = 'flex';
+      //document.getElementById('waitingForEmailToBeSentIndicatorFromAuth').style.justifyContent = 'center';
+      //document.getElementById('loginButton').disabled = 'true';
+      const { user, supabaseSession, error } = await supabase.auth.signIn({
+         email: email,
+         password: password
       });
       if(error){
          alert(error.message)
       }else{
-         document.getElementById('waitingForEmailToBeSentIndicatorFromAuth').style.display = 'none';
-         document.getElementById('emailSentIndicatorFromAuth').style.display = 'block'
+         $session = supabaseSession
       }
    }
 
@@ -31,27 +31,12 @@
    
 </script>
 
-<div id="loginFromPage">
-   <h1>Login</h1>
-   <p>Fill in your email address to login or signup!</p>
-   <br>
-   <form class="" on:submit|preventDefault={signIn}> 
-      <input type="text" class="textForm" placeholder="Email address" required="required" bind:value={email}>
-      <button class="buttonAuth" type="submit">Sign in</button>
-   </form>
-   <br>
-   <p>An email with a magic link be sent to your inbox: just open that link and you'll be logged into Otium!</p>
-   <p>Your email won't be shared with anyone.</p>
-</div>
-<div id="waitingForEmailToBeSentIndicatorFromAuth" class="loader"><Jumper size="120" color="#f2b3cf" unit="px" duration="1s"></Jumper></div>
-<h1 style="text-align: center;" id="emailSentIndicatorFromAuth">Email sent!</h1>
-
-
-<style>
-   form{
-      text-align: center;
-   }
-   #emailSentIndicatorFromAuth, #waitingForEmailToBeSentIndicatorFromAuth{
-     display: none;
-   }
- </style>
+const { user, supabaseSession, error } = await supabase.auth.signIn({
+         email: email,
+         password: password
+      });
+      if(error){
+         alert(error.message)
+      }else{
+         $session = supabaseSession
+      }
---
+++ src/routes/signup.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { session } from '$app/stores';
+    import supabase from '$lib/db';
+    let email, password;
+    async function signUp(){
+        const { user, supabaseSession, error } = await supabase.auth.signUp({
+         email: email,
+         password: password
+        });
+        if(error){
+            alert(error.message)
+        }else{
+            if(user){
+                $session = supabaseSession
+            }
+        }
+    }
+</script>
+
+<div id="loginFromPage">
+    <h1>Login</h1>
+    <form class="" on:submit|preventDefault={signUp}> 
+       <input type="email" class="textForm" placeholder="Email address" required="required" bind:value={email}>
+       <input type="password" class="textForm" placeholder="Password" required="required" bind:value={password}>
+       <button class="buttonAuth" type="submit">Sign UP</button>
+    </form>
+ </div>
+ 
+ <style>
+    form{
+       text-align: center;
+    }
+  </style>
