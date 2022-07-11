1ebf5b526a5e3bcf3323eb502cb472511160bb1b
alombi <alombi@hotmail.com>
2022-07-11 17:53:14 +0200

    Makes the release_notes page prettier

--- src/routes/release_notes.svelte
+++ src/routes/release_notes.svelte
@@ -15,48 +15,66 @@
 
 <h2>8 June 2022</h2>
 <ul>
-   <li>(NEW) A page for tracking Otium's progress and new features is here!</li>
-   <li>(NEW) A new about section in the sidebar</li>
+   <li><span class="release-tag new">NEW</span> A page for tracking Otium's progress and new features is here!</li>
+   <li><span class="release-tag new">NEW</span> A new about section in the sidebar</li>
 </ul>
 
 <h2>29 March 2022</h2>
 <ul>
-   <li>(NEW) Hello Reading flows!</li>
-   <li>(FIX) New icons and metadata</li>
-   <li>(FIX) Overall modals improvements</li>
+   <li><span class="release-tag new">NEW</span> Hello Reading flows!</li>
+   <li><span class="release-tag fix">FIX</span> New icons and metadata</li>
+   <li><span class="release-tag fix">FIX</span> Overall modals improvements</li>
 </ul>
 
 <h2>23 March 2022</h2>
 <ul>
-   <li>(NEW) Notifications are now a thing!</li>
-   <li>(FIX) Improvements to authentication</li>
-   <li>(FIX) Home page redesign</li>
-   <li>(FIX) Various improvements updates</li>
+   <li><span class="release-tag new">NEW</span> Notifications are now a thing!</li>
+   <li><span class="release-tag fix">FIX</span> Improvements to authentication</li>
+   <li><span class="release-tag fix">FIX</span> Home page redesign</li>
+   <li><span class="release-tag fix">FIX</span> Various improvements updates</li>
 </ul>
 
 <h2>17 March 2022</h2>
 <ul>
-   <li>(NEW) Friendship and common books are now available</li>
-   <li>(FIX) Massive improvements to mobile UI</li>
+   <li><span class="release-tag new">NEW</span> Friendship and common books are now available</li>
+   <li><span class="release-tag fix">FIX</span> Massive improvements to mobile UI</li>
 </ul>
 
 <h2>26 February 2022</h2>
 <ul>
-   <li>(NEW) Bookshelf are now a feature!</li>
-   <li>(FIX) Updated to latest SvelteKit changes</li>
-   <li>(FIX) Various login problems are fixed</li>
-   <li>(FIX) Fixed a problem with data not fetched after login</li>
+   <li><span class="release-tag new">NEW</span> Bookshelf are now a feature!</li>
+   <li><span class="release-tag fix">FIX</span> Updated to latest SvelteKit changes</li>
+   <li><span class="release-tag fix">FIX</span> Various login problems are fixed</li>
+   <li><span class="release-tag fix">FIX</span> Fixed a problem with data not fetched after login</li>
 </ul>
 
 <h2>29 December 2021</h2>
 <ul>
-   <li>(NEW) Searching a book is now possible and very fast</li>
-   <li>(NEW) Created Otium API</li>
-   <li>(NEW) New style of various part of the website, including home and books' pages</li>
-   <li>(FIX) Fixed all the problems with deployment: the webiste is now live</li>
+   <li><span class="release-tag new">NEW</span> Searching a book is now possible and very fast</li>
+   <li><span class="release-tag new">NEW</span> Created Otium API</li>
+   <li><span class="release-tag new">NEW</span> New style of various part of the website, including home and books' pages</li>
+   <li><span class="release-tag fix">FIX</span> Fixed all the problems with deployment: the webiste is now live</li>
 </ul>
 
 <h2>7 November 2021</h2>
 <ul>
    <li>Otium development begins!</li>
 </ul>
+
+<style>
+   .release-tag{
+      padding:2px 5px;
+      border-radius: 4px;
+   }
+   .new{
+      background-color: #4BB89A1a;
+      color:#4BB89A
+   }
+   .fix{
+         background-color: #bfd4f21a;
+         color: #bfd4f2;
+   }
+   li{
+      margin-top:8px;
+   }
+</style>
