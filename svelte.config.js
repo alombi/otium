a6ec3f759327f187e6eab609c4e5f3860571dee9
alombi <alombi@hotmail.com>
2022-12-04 12:34:38 +0100

    Fixes an error

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter:vercel(),
 	}
 };
 
